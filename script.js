8aee5ddbbc1d1bc4ff9b3cb35b268317743ecfbf
Kyle Klaiber <Kylesklaiber@gmail.com>
2018-11-30 16:34:54 -0800

    Lecture 63: Object.create and Lecture 64: Primitives and Objects

--- script.js
+++ script.js
@@ -32,6 +32,88 @@ john.calAge();
 billy.calAge();
 susie.calAge();
 
+
+
+
+//Lecture 63 Object.create
+
+var personProto = {
+    calcAge: function(){
+        console.log(2018 - this.yob);
+    }
+};
+
+var bobby = Object.create(personProto);
+bobby.name = "Bobby",
+bobby.yob = 1981,
+bobby.job = "Bus Driver"
+
+var jane = Object.create(personProto,
+{
+    name: { value: 'Jane'},
+    yob: {value: 1978},
+    job: { value: 'Chef'}
+})
+
+
+
+
+//Lecture 64 Primitives vs Objects
+
+// Variables containing primitives (ie, strings, numbers, booleans, undefined, null)
+// actually hold that data inside the variable itself. Variables associated with objects
+// don't actually contain the object, instead they contain a reference to the place
+// in memory where the object sits, where the object is stored. in other words, the 
+// var points to where the object is stored.
+
+//Primitives
+var a = 23;
+var b = a;
+a = 46;
+
+console.log(a,b)
+//Since b is not changed when we change a after declaring b, it shows that each var
+//actually hold a copy of the data, they dont reference anything
+
+
+//Objects
+var obj1 = {
+    name: 'Riley',
+    age: 2
+};
+var obj2 = obj1;
+
+obj1.age = 4;
+console.log(obj1.age, obj2.age)
+//But here when we change obj.age to 4 it actually changes obj2's age to 4 as well.
+// it's because i didnt actually make a new object with obj2, it was only referencing,
+// or pointing to obj1
+
+
+//Functions
+
+var age = 34;
+var obj = {
+    name: 'Tyler',
+    city: 'New York City'
+};
+
+function change(a, b){
+    a = 45,
+    b.city = "Redding"
+}
+
+change(age, obj);
+console.log(age, obj.city);
+//Here we passed a primitive and a object into the change function...but you can see
+// that only the city was changed because when we pass the primitive into the function
+// a simple copy is created, so we can change a (or a primitive) as much as we want 
+// and it will never affect the var on the outside. But when we pass the object in, its 
+// not really the object that we pass into the function but the REFERENCE 
+// that POINTS to the object. SO when we change to object inside of the function it is
+// still reflected outside of the function
+
+
 //Section 5 Lecture 67
 //IIFE
 
