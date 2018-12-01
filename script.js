74b99c22ea9c62fb4cbfd517360b450711bb6614
Kyle Klaiber <Kylesklaiber@gmail.com>
2018-11-30 23:18:44 -0800

    Lecture 65: Passing Functions As Arguments

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ var Person = function(name, yearOfBirth, job) {
   this.name = name;
   this.yearOfBirth = yearOfBirth;
   this.job = job;
-  
 };
 
 //The protoype property enables us to use inheritance. Here we give all the instances
@@ -19,11 +18,11 @@ var Person = function(name, yearOfBirth, job) {
 //have the calAge function in it, the new instances all have access to it because of
 //inheritance from the prototype property
 Person.prototype.calAge = function() {
-  console.log( 2018 - this.yearOfBirth + ' years is ' + this.name + '\'s age');
+  //   console.log( 2018 - this.yearOfBirth + ' years is ' + this.name + '\'s age');
 };
 
 //You can also add properties to the prototype property.
-Person.prototype.lastName = 'Smith';
+Person.prototype.lastName = "Smith";
 
 var john = new Person("John", 1982, "Plumber");
 var billy = new Person("Billy", 1988, "Teacher");
@@ -32,38 +31,29 @@ john.calAge();
 billy.calAge();
 susie.calAge();
 
-
-
-
 //Lecture 63 Object.create
 
 var personProto = {
-    calcAge: function(){
-        console.log(2018 - this.yob);
-    }
+  calcAge: function() {
+    // console.log(2018 - this.yob);
+  }
 };
 
 var bobby = Object.create(personProto);
-bobby.name = "Bobby",
-bobby.yob = 1981,
-bobby.job = "Bus Driver"
+(bobby.name = "Bobby"), (bobby.yob = 1981), (bobby.job = "Bus Driver");
 
-var jane = Object.create(personProto,
-{
-    name: { value: 'Jane'},
-    yob: {value: 1978},
-    job: { value: 'Chef'}
-})
-
-
-
+var jane = Object.create(personProto, {
+  name: { value: "Jane" },
+  yob: { value: 1978 },
+  job: { value: "Chef" }
+});
 
 //Lecture 64 Primitives vs Objects
 
 // Variables containing primitives (ie, strings, numbers, booleans, undefined, null)
 // actually hold that data inside the variable itself. Variables associated with objects
 // don't actually contain the object, instead they contain a reference to the place
-// in memory where the object sits, where the object is stored. in other words, the 
+// in memory where the object sits, where the object is stored. in other words, the
 // var points to where the object is stored.
 
 //Primitives
@@ -71,47 +61,90 @@ var a = 23;
 var b = a;
 a = 46;
 
-console.log(a,b)
+// console.log(a,b)
 //Since b is not changed when we change a after declaring b, it shows that each var
 //actually hold a copy of the data, they dont reference anything
 
-
 //Objects
 var obj1 = {
-    name: 'Riley',
-    age: 2
+  name: "Riley",
+  age: 2
 };
 var obj2 = obj1;
 
 obj1.age = 4;
-console.log(obj1.age, obj2.age)
+// console.log(obj1.age, obj2.age)
 //But here when we change obj.age to 4 it actually changes obj2's age to 4 as well.
 // it's because i didnt actually make a new object with obj2, it was only referencing,
 // or pointing to obj1
-
 
 //Functions
 
 var age = 34;
 var obj = {
-    name: 'Tyler',
-    city: 'New York City'
+  name: "Tyler",
+  city: "New York City"
 };
 
-function change(a, b){
-    a = 45,
-    b.city = "Redding"
+function change(a, b) {
+  (a = 45), (b.city = "Redding");
 }
 
 change(age, obj);
-console.log(age, obj.city);
+// console.log(age, obj.city);
 //Here we passed a primitive and a object into the change function...but you can see
 // that only the city was changed because when we pass the primitive into the function
-// a simple copy is created, so we can change a (or a primitive) as much as we want 
-// and it will never affect the var on the outside. But when we pass the object in, its 
-// not really the object that we pass into the function but the REFERENCE 
+// a simple copy is created, so we can change a (or a primitive) as much as we want
+// and it will never affect the var on the outside. But when we pass the object in, its
+// not really the object that we pass into the function but the REFERENCE
 // that POINTS to the object. SO when we change to object inside of the function it is
 // still reflected outside of the function
+
+//Lecture 65 Passing Functions as Arguments
+
+//  A function is an instance of an Object type. And because of that it behaves just like
+//  any other Object. We can store functions in a variable. We can pass a function
+//  as an argument to another function. And we can also return a function from a function
+//  . And because of all of this, in JS we say these are First-Class functions.
+
+var years = [1991, 1999, 2001, 1984, 1979, 1951, 1944];
+
+function arrayCal(arr, fn){
+    var arrayRes = [];
+    for(var i = 0; i < arr.length; i++){
+        arrayRes.push(fn(arr[i]))
+    }
+    return arrayRes;
+}
+
+function calculateA(el){
+    return 2018 - el;
+}
+
+function isFullAge(el){
+   return el >= 18
+}
+
+function oldEnoughToRetire(el){
+    return el >= 65;
+}
+
+function maxHeartRate(el){
+    if(el >= 18 && el <= 81){
+        return Math.round(206.9 - (0.67 * el))
+    }else{
+        return -1;
+    }
+}
+var ages = arrayCal(years, calculateA);
+var retirementAge = arrayCal(ages, oldEnoughToRetire);
+// console.log(retirementAge);
+var rates = arrayCal(ages, maxHeartRate);
+// console.log(rates);
+var fullAges = arrayCal(years, isFullAge);
+// console.log(fullAges);
+// console.log(ages);
+
 
 
 //Section 5 Lecture 67
