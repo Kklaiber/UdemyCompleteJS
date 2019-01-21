3a34f0d7ae537a0ec378111122ca3de61e751195
Kyle Klaiber <Kylesklaiber@gmail.com>
2019-01-20 20:07:27 -0800

    Delete item from UI lecture 92

--- BudgetApp/app.js
+++ BudgetApp/app.js
@@ -154,6 +154,14 @@ var UIController = (function() {
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
     },
 
+    deleteListItem: function(selectorID){
+      
+      var el = document.getElementById(selectorID);
+
+      el.parentNode.removeChild(el);
+
+    },
+
     clearFields: function(){
         var fields, fieldsArr;
 
@@ -256,10 +264,10 @@ var controller = (function(budgetCtrl, UICtrl) {
       // //1. Delete Item from data structure
       budgetCtrl.deleteItem(type, ID);
       // //2. Delete Item from UI
-  
-      // //3. Recalculate Budget
-  
-      // //4. Update the UI
+      UICtrl.deleteListItem(itemID);
+      //3. Update and show new Budget
+      updateBudget();
+
     };
     
 
