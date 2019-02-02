9d17139976897cf59ce7a6b3e466789bd7d99978
Kyle Klaiber <Kylesklaiber@gmail.com>
2019-02-01 23:44:54 -0800

    LECTURE 108 ARROW FUNCTIONS Lexical THIS keyword

--- Walkthrough/Section7ModernJS/script.js
+++ Walkthrough/Section7ModernJS/script.js
@@ -148,3 +148,96 @@ ages6 = years.map((el, index, arr) => {
 
 })
 // console.log(ages6)
+
+
+
+//LECTURE 108 ARROW FUNCTIONS Lexical THIS keyword
+
+//ES5
+
+var box5 = {
+    color: 'green',
+    position: 1,
+    clickMe: function(){
+
+        var self = this;
+        document.querySelector('.green').addEventListener('click',function(){
+            var str = 'This is box number ' + self.position + ' and it is ' + self.color;
+            alert(str);
+        })
+    }
+}
+
+box5.clickMe();
+
+
+//ES6
+
+const box6 = {
+    color: 'green',
+    position: 1,
+    clickMe: function(){
+
+        document.querySelector('.green').addEventListener('click',() => {
+            var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+            alert(str);
+        })
+    }
+}
+
+// box6.clickMe();
+
+
+
+
+const box66 = {
+    color: 'green',
+    position: 1,
+    clickMe: function(){
+
+        document.querySelector('.green').addEventListener('click',() => {
+            var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+            alert(str);
+        })
+    }
+}
+
+box66.clickMe();
+
+
+function Person(name){
+    this.name = name;
+}
+
+Person.prototype.myFriends5 = function(friends){
+    var arr = friends.map(function(el)
+    {
+        return this.name + " is friends with " + el
+    }.bind(this))
+
+    console.log(arr)
+}
+
+friends = ["bob", "henry", "frank"];
+
+new Person('Kyle').myFriends5(friends);
+
+
+//ES6
+
+function Person(name){
+    this.name = name;
+}
+
+Person.prototype.myFriends6 = function(friends){
+    var arr = friends.map(el => 
+    
+         `${this.name} is friends with ${el}`
+    );
+
+    console.log(arr)
+}
+
+friends = ["bob", "henry", "frank"];
+
+new Person('Jerry').myFriends6(friends);
