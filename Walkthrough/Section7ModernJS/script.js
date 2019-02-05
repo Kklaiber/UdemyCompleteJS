34290c83628cbb5874a92cf03e13728165fd5a2a
Kyle Klaiber <Kylesklaiber@gmail.com>
2019-02-04 16:40:54 -0800

    Lecture 112 Rest Parameters

--- Walkthrough/Section7ModernJS/script.js
+++ Walkthrough/Section7ModernJS/script.js
@@ -140,12 +140,6 @@ ages6 = years.map((el, index, arr) => {
 });
 // console.log(ages6)
 
-
-
-
-
-
-
 //LECTURE 108 ARROW FUNCTIONS Lexical THIS keyword
 
 //ES5
@@ -229,12 +223,6 @@ friends = ["bob", "henry", "frank"];
 
 new Person("Jerry").myFriends6(friends);
 
-
-
-
-
-
-
 //LECTURE 109: DESTRUCTURING
 
 //ES5
@@ -266,68 +254,49 @@ const [age2, yearTilRetirement] = calcAgeRetirement(1984);
 // console.log(age2);
 // console.log(yearTilRetirement);
 
-
-
-
-
-
-function Person(names, ages, weight){
-    this.names = names;
-    this.ages = ages;
-    this.weight = weight;
+function Person(names, ages, weight) {
+  this.names = names;
+  this.ages = ages;
+  this.weight = weight;
 }
 
-const jerry = new Person('Jerry', 19, '200lbs');
+const jerry = new Person("Jerry", 19, "200lbs");
 
 // console.log(`My name is ${this.names}, I am ${this.ages} years old and I weigh ${this.weight}`);
 
-
-
-
-
-
-
-
 //LECTURE 110: ARRAYS
 
-const boxes = document.querySelectorAll('.box');
+const boxes = document.querySelectorAll(".box");
 
 //ES5
 var boxesArr5 = Array.prototype.slice.call(boxes);
 
-boxesArr5.forEach(function(cur){
-    cur.style.backgroundColor = 'dodgerblue'
+boxesArr5.forEach(function(cur) {
+  cur.style.backgroundColor = "dodgerblue";
 });
-
-
 
 //ES6
 var boxesArr6 = Array.from(boxes);
 
-boxesArr6.forEach(cur => cur.style.backgroundColor = 'blue');
-
+boxesArr6.forEach(cur => (cur.style.backgroundColor = "blue"));
 
 //ES5
-for(var i = 0; i < boxesArr5.length; i++){
-    if(boxesArr5[i].className === 'box blue'){
-        boxesArr5[i].textContent = "I'm blue";
-        boxesArr5[i].style.backgroundColor = "blue"
-        continue;
-    } 
-    boxesArr5[i].textContent = "I'm yellow"
+for (var i = 0; i < boxesArr5.length; i++) {
+  if (boxesArr5[i].className === "box blue") {
+    boxesArr5[i].textContent = "I'm blue";
+    boxesArr5[i].style.backgroundColor = "blue";
+    continue;
+  }
+  boxesArr5[i].textContent = "I'm yellow";
 }
-
 
 //ES6
-for(const i of boxesArr6){
-    if(i.className.includes('blue')){
-        continue;
-    }
-    i.textContent = 'I changed to blue';
+for (const i of boxesArr6) {
+  if (i.className.includes("blue")) {
+    continue;
+  }
+  i.textContent = "I changed to blue";
 }
-
-
-
 
 // const boxx = document.querySelectorAll('.box');
 // const boxArr6 = Array.from(boxx);
@@ -339,76 +308,103 @@ for(const i of boxesArr6){
 //     cur.textContent = 'I changed to bluuuue'
 // }
 
-
 //ES5
 
 var ages = [1, 6, 11, 14, 19, 8];
 
-var fullAge = ages.map(function(cur){
-    return cur >= 18
-})
+var fullAge = ages.map(function(cur) {
+  return cur >= 18;
+});
 // console.log(fullAge)
 // console.log(fullAge.indexOf(true))
 // console.log(ages[fullAge.indexOf(true)])
-
 
 //ES6
 
 // console.log(ages.findIndex(cur => cur >= 18));
 // console.log(ages.find(cur => cur >= 18))
 
-
-
-
-
-
-
 //LECTURE 111: SPREAD OPERATOR
 
-function addFourAges(a, b, c, d){
-    return a + b + c + d;
+function addFourAges(a, b, c, d) {
+  return a + b + c + d;
 }
 
 var tot1 = addFourAges(10, 33, 23, 44);
 // console.log(tot1)
 
 //ES5
-var ages = [10, 33, 23,44];
+var ages = [10, 33, 23, 44];
 
 var tot2 = addFourAges.apply(null, ages);
 // console.log(tot2);
 
-//ES6 
+//ES6
 let tot3 = addFourAges(...ages);
 // console.log(tot3)
 
+var afcTeams = ["Browns", "Steelers", "Ravens", "Bengals"];
+var nfcTeams = ["Chargers", "Rams", "Raiders", "Seahawks"];
 
-
-var afcTeams = ['Browns', 'Steelers', 'Ravens', 'Bengals'];
-var nfcTeams = ['Chargers', 'Rams', 'Raiders', 'Seahawks'];
-
-var playoffs = [...afcTeams, ...nfcTeams, 'Patriots'];
+var playoffs = [...afcTeams, ...nfcTeams, "Patriots"];
 
 // console.log(playoffs);
 
-
-const h = document.querySelector('h1');
-const bx = document.querySelectorAll('.box');
+const h = document.querySelector("h1");
+const bx = document.querySelectorAll(".box");
 
 const all = [h, ...bx];
-Array.from(all).forEach(cur => cur.style.textTransform = 'uppercase');
-Array.from(bx).forEach(cur => cur.style.color = 'white');
-h.style.color = 'gray'
+Array.from(all).forEach(cur => (cur.style.textTransform = "uppercase"));
+Array.from(bx).forEach(cur => (cur.style.color = "white"));
+h.style.color = "gray";
 
+function records(wins, loss, tie) {
+  var perc = Math.round((wins / (wins + loss)) * 100).toFixed(2);
+  // console.log(`The Browns record is W-${wins} L-${loss} T-${tie} Percent- ${perc}%`);
 
-
-function records(wins, loss, tie){
-    var perc = Math.round(wins / (wins + loss) * 100).toFixed(2) ;
-    // console.log(`The Browns record is W-${wins} L-${loss} T-${tie} Percent- ${perc}%`);
-
-    return perc;
-
+  return perc;
 }
 
-const browns = records(5,11,1 )
+const browns = records(5, 11, 1);
 
+//LECTURE 112: REST PARAMETERS
+//ES5
+// function isFullAge5(){
+//     var argsArr = Array.prototype.slice.call(arguments);
+
+//     argsArr.forEach(function(cur){
+//         console.log((2019 - cur) >= 18);
+//     })
+// }
+
+// isFullAge5(1990, 1988, 2011, 2000)
+
+//ES6
+// function isFullAge6(...yrs){
+//         yrs.forEach(cur => console.log((2019 - cur) >= 18))
+// }
+
+// isFullAge6(1990, 2011, 2002, 1999);
+
+function isFullAge5(limit) {
+  var argsArr = Array.prototype.slice.call(arguments, 1);
+
+  argsArr.forEach(function(cur) {
+    // console.log((2019 - cur) >= limit);
+  });
+}
+
+isFullAge5(5, 1990, 1988, 2011, 2000);
+
+//ES6
+function isFullAge6(limit, ...yrs) {
+  yrs.forEach(cur => console.log(2019 - cur >= limit));
+}
+
+isFullAge6(21, 1990, 2011, 2002, 1989);
+
+function emotions(joy, ...mood) {
+  mood.forEach(cur => console.log(cur === joy));
+}
+
+emotions("happy", "mad", "happy", "sad", "glad", "happy");
